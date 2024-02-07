70ece81e39da7fbaf3402e72e27ce06aadcedf79
sdac-5 <sdac-5@sdac-5-PC>
2024-02-07 19:45:24 +0530

    hi

--- Hello/src/Main.java
+++ Hello/src/Main.java
@@ -6,5 +6,6 @@ public static void main(String[] args) {
 	System.out.println("Hey Buddy2");
 	System.out.println("Hey Buddy3");
 	System.out.println("Hey Buddy4");
+	System.out.println("Hey Buddy");
 }
 }
