7d964315b38ba0a4d86590470ef83cd54c44f660
sdac-5 <sdac-5@sdac-5-PC>
2024-02-07 19:35:40 +0530

    commit3

--- Hello/src/Main.java
+++ Hello/src/Main.java
@@ -4,5 +4,7 @@ public static void main(String[] args) {
 	System.out.println("Hey Buddy");
 	System.out.println("Hey Buddy1");
 	System.out.println("Hey Buddy2");
+	System.out.println("Hey Buddy3");
+	System.out.println("Hey Buddy4");
 }
 }
